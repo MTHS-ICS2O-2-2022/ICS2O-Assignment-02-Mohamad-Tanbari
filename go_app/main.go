b90c7ca9797ecd929e0a4f5a78476a931838ec3d
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-03-24 14:23:25 +0000

    changed picture

--- go_app/main.go
+++ go_app/main.go
@@ -1,7 +1,7 @@
 // Made by: Mohamad
 // Date: Mar 23 2023
 //
-// This script calculates the area of an isosagon.
+// This script calculates the area of an icosagon.
 
 package main
 
@@ -16,14 +16,14 @@ func main() {
 	sides := 20
 
 	// get user input
-	fmt.Println("Enter the length of the side of the isosagon: ")
+	fmt.Println("Enter the length of the side of the icosagon: ")
 	fmt.Scanln(&sideLength)
 
 	// calculate area
 	area := (float64(sides) * math.Pow(sideLength, 2)) / (4 * math.Tan(math.Pi/float64(sides)))
 
 	// Output result
-	fmt.Println("The area of the isosagon is: ", area, "cm²")
+	fmt.Println("The area of the icosagon is: ", area, "cm²")
 
 	fmt.Println("Done.")
 }
